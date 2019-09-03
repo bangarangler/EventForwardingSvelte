c0fbc88c729fa01c77ab7d62f69d2009379158b6
Jonathan Dain Palacio <bangarangler@gmail.com>
2019-09-03 09:54:58 -0400

    shortcut for passing props and default props

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,29 @@
 <script>
 	import Product from './Product.svelte';
+
+	let products = [
+		{
+			id: 'p1',
+			title: 'A book',
+			price: 9.99
+		}
+	]
+
+	function addToCart(event) {
+		console.log(event)
+	}
+
+	function deleteProduct(event) {
+		console.log(event.detail)
+	}
 </script>
 
+{#each products as product}
 <Product
-	productTitle="A Book"
- on:add-to-cart={() => alert('Add to Cart')}
- on:delete={() => alert('Delete')}
+	{...product}
+ on:add-to-cart={addToCart}
+ on:delete={deleteProduct}
  />
+{/each}
 
 
