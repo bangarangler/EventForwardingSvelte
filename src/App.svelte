473a45b74d49ad500e3722f8641d8e43e4606303
Jonathan Dain Palacio <bangarangler@gmail.com>
2019-09-03 09:37:32 -0400

    emit your own custom events

--- src/App.svelte
+++ src/App.svelte
@@ -2,6 +2,10 @@
 	import Product from './Product.svelte';
 </script>
 
-<Product productTitle="A Book" on:click={() => alert('Clicked')} />
+<Product
+	productTitle="A Book"
+ on:add-to-cart={() => alert('Add to Cart')}
+ on:delete={() => alert('Delete')}
+ />
 
 
